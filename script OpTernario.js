39f7f9d54018a38412077c95c70363c2ca1277b9
Ivan-Snts <67763923+Ivan-Snts@users.noreply.github.com>
2022-05-16 18:26:12 -0300

    Add files via upload

---
+++ script OpTernario.js
@@ -0,0 +1,7 @@
+var idade = 15;
+
+//idade >= 18 ? console.log("Pode") : console.log ("Não Pode")
+
+var pode = idade >= 18 ? true: false;
+
+console.log("Pode");
---
+++ script switch.js
@@ -0,0 +1,44 @@
+var nota1 = 5.0;
+var nota2 = 6.0;
+
+var media = ( nota1 + nota2) /2;
+
+var conceito =""
+
+if( media >= 8){
+  
+    conceito = "ótimo";
+  
+}
+
+else if (media >= 6.5){
+
+    conceito = "Bom";
+    
+}else {
+
+    conceito ="Regular";
+
+}
+
+console.log(media);
+console.log(conceito);
+
+switch(conceito){
+
+    case "ótimo":
+        console.log("Parabéns você é ótimo aluno")
+        break;
+
+    case "Bom":
+        console.log("Parabéns você está no caminho certo")
+        break;
+
+    case "Regular":
+        console.log("Estude um pouco mais")
+        break;
+
+    default:
+        console.log("Houve algum erro")
+        break;
+}
